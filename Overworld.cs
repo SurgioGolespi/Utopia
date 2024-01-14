c0cf0c3ee4291dbde33dca8b3fb8c777fe487581
SurgioGolespi <ssamgajula@gmail.com>
2024-01-14 15:46:01 -0800

    Commit 2

--- Overworld.cs
+++ Overworld.cs
@@ -1,21 +1,38 @@
 using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 public class Overworld : MonoBehaviour{
-    public Transform[] Party;
-    public Transform[] Foreground;
+    public SpriteRenderer[] Party;
+    public SpriteRenderer[] Foreground;
+    public Sprite[] FGSprite;
     public Camera MainCam;
     public float Horizontal;
+    public float Vertical;
     void Update(){
         TouchScreen();
-        Party[0].transform.Translate(Horizontal * Time.deltaTime * 10, 0, 0);
-        Party[0].localScale = (Horizontal != 0) ? new Vector3(Horizontal, 1, 1) : new Vector3(Party[0].localScale.x, 1, 1);
-        MainCam.transform.position = Party[0].position + new Vector3(4, 2.2f, -10);
-        foreach(Transform i in Foreground){
-            i.position = ((Party[0].position.x - i.position.x) * Horizontal >= 40) ? new Vector3(i.position.x + 66 * Horizontal, 0, 0): i.position;}}
+        Party[0].transform.Translate(Horizontal * Time.deltaTime * 15, 0, 0);
+        Party[1].transform.position = Party[0].transform.position + new Vector3(-2 * Party[0].transform.localScale.x, 0, 0);
+        Party[2].transform.position = Party[0].transform.position + new Vector3(-4 * Party[0].transform.localScale.x, 0, 0);
+        Party[0].transform.localScale = (Horizontal != 0) ? new Vector3(Horizontal, 1, 1) : new Vector3(Party[0].transform.localScale.x, 1, 1);
+        Party[1].transform.localScale = new Vector3(Party[0].transform.localScale.x, 1, 1);
+        Party[2].transform.localScale = new Vector3(Party[0].transform.localScale.x, 1, 1);
+        MainCam.transform.position = Party[0].transform.position + new Vector3(4, 2.2f, -10);
+        foreach(SpriteRenderer i in Foreground){
+            i.transform.position = ((Party[0].transform.position.x - i.transform.position.x) * Horizontal >= 40) 
+            ? new Vector3(i.transform.position.x + 66 * Horizontal, 0, 0): i.transform.position;
+        if(Party[0].transform.position.x >= 500){
+            ChangeArea();}}}
     public void TouchScreen(){
         if(Input.touchCount > 0){
             if(Input.GetTouch(0).phase == TouchPhase.Moved){
-                Horizontal = (Mathf.Abs(Input.GetTouch(0).deltaPosition.x) > 5) ? Mathf.Sign(Input.GetTouch(0).deltaPosition.x) : Horizontal;}}
+                Horizontal = (Mathf.Abs(Input.GetTouch(0).deltaPosition.x) > 5) ? Mathf.Sign(Input.GetTouch(0).deltaPosition.x) : Horizontal;
+                Vertical = (Mathf.Abs(Input.GetTouch(0).deltaPosition.y) > 5) ? Mathf.Sign(Input.GetTouch(0).deltaPosition.y) : Vertical;}}
         else{
-            Horizontal = 0;}}}
+            Horizontal = 0;
+            Vertical = 0;}}
+    public void ChangeArea(){
+        foreach(SpriteRenderer i in Foreground){
+            Party[0].transform.position = new Vector3(0,0,0);
+            i.transform.position += new Vector3(-500,0,0);
+            i.sprite = FGSprite[(Array.IndexOf(FGSprite, i.sprite) + 1) % 3];}}}
